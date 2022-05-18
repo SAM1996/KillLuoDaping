9f8dc7020ca4104801be5492c6f25f71d14daff5
SAM1996 <samuelgao1996@gmail.com>
2022-05-18 23:29:20 +0800

    init

---
+++ LdpTypes.h
@@ -0,0 +1,11 @@
+#ifndef _LDP_TYPES_H
+#define _LDP_TYPES_H
+
+typedef unsigned char uint8;      /* 无符号8位数据  */
+typedef signed char int8;         /* 有符号8位数据  */
+typedef unsigned short uint16;    /* 无符号16位数据 */
+typedef signed short int16;       /* 有符号16位数据   */
+typedef unsigned int uint32;      /* 无符号32位数据 */
+typedef signed int int32;         /* 有符号32位数据   */
+
+#endif
